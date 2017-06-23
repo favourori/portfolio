b4469214aadb2de756d985fd2bae0206489d28c5
Favour Ori <orifavour2000@gmail.com>
2017-06-23 19:12:40 -0400

    A few updates ( new Page -> Works & associated CSS )

--- js/myjs.js
+++ js/myjs.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     UIkit.modal(element, options);
 
 
+
 });
 
 
--- webCv/js/myjava.js
+++
@@ -1,5 +0,0 @@
-jQuery('.skillbar').each(function(){
-    jQuery(this).find('.skillbar-bar').animate({
-        width:jQuery(this).attr('data-percent')
-    },2000);
-});
